b041bef25ed1c6613586e078e104e3b3543a00b4
Carl <Carl@example.com>
2018-09-06 10:10:06 +0200

    Combined Admin controller PUT and POST methods(create and update) for class as you create the id some the method is the same.

--- src/main/java/za/co/travelstart/TravelstartWebAppServer/controller/AdminController.java
+++ src/main/java/za/co/travelstart/TravelstartWebAppServer/controller/AdminController.java
@@ -130,14 +130,8 @@ public class AdminController {
         throw new RuntimeException("Airport Not Found");
     }
 
-    // Create new class
-    @RequestMapping(path = "/class", method = RequestMethod.POST, consumes = "application/json")
-    public void saveClass(@RequestBody Class _class){
-        flightAdminService.saveClass(_class);
-    }
-
-    // Edit existing class
-    @RequestMapping(path = "/class/{airplaneId}/{className}", method = RequestMethod.PUT, consumes = "application/json")
+    // Create and Edit existing class
+    @RequestMapping(path = "/class/{airplaneId}/{className}", method = {RequestMethod.PUT, RequestMethod.POST}, consumes = "application/json")
     public void editAirport(@PathVariable("airplaneId") Long airplaneId, @PathVariable("className") String className,
                             @RequestBody Class _class){
         _class.setClassId(new ClassId(airplaneId, className));
